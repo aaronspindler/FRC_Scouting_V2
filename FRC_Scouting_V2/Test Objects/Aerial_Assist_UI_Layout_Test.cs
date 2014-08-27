aa834ce1ee31286ce06aa73bf069ce2e8716beff
xNovax <nova1104@live.com>
2014-08-27 18:47:38 -0400

    Added some new folders and an item to test out the layout for Aerial_Assist scouting events

---
+++ FRC_Scouting_V2/Test Objects/Aerial_Assist_UI_Layout_Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FRC_Scouting_V2.Test_Items
+{
+    public partial class Aerial_Assist_UI_Layout_Test : UserControl
+    {
+        public Aerial_Assist_UI_Layout_Test()
+        {
+            InitializeComponent();
+        }
+    }
+}
