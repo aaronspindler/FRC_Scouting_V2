e248543feedaa9ec60d4c2102a77d419385e8011
xNovax <nova1104@live.com>
2014-08-28 20:53:44 -0400

    More work on the AerialAssist UI

--- FRC_Scouting_V2/Test Objects/Aerial_Assist_UI_Layout_Test.Designer.cs
+++ FRC_Scouting_V2/Test Objects/Aerial_Assist_UI_Layout_Test.Designer.cs
@@ -33,6 +33,20 @@
             this.teamColourComboBox = new System.Windows.Forms.ComboBox();
             this.matchNumberLabel = new System.Windows.Forms.Label();
             this.matchNumberNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.submitDataButton = new System.Windows.Forms.Button();
+            this.controlledHighButton = new System.Windows.Forms.Button();
+            this.controlledLowButton = new System.Windows.Forms.Button();
+            this.autoLowButton = new System.Windows.Forms.Button();
+            this.autoHighButton = new System.Windows.Forms.Button();
+            this.autonymousHighLabel = new System.Windows.Forms.Label();
+            this.autonymousLowLabel = new System.Windows.Forms.Label();
+            this.controlledHighLabel = new System.Windows.Forms.Label();
+            this.controledLowLabel = new System.Windows.Forms.Label();
+            this.autoHighValueDisplay = new System.Windows.Forms.Label();
+            this.autoLowValueDisplay = new System.Windows.Forms.Label();
+            this.controlledHighValueDisplay = new System.Windows.Forms.Label();
+            this.controlledLowValueDisplay = new System.Windows.Forms.Label();
+            this.scoringLabel = new System.Windows.Forms.Label();
             this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.matchNumberNumericUpDown)).BeginInit();
             this.SuspendLayout();
@@ -60,6 +74,20 @@
             this.tableLayoutPanel1.Controls.Add(this.teamColourComboBox, 3, 0);
             this.tableLayoutPanel1.Controls.Add(this.matchNumberLabel, 8, 0);
             this.tableLayoutPanel1.Controls.Add(this.matchNumberNumericUpDown, 11, 0);
+            this.tableLayoutPanel1.Controls.Add(this.submitDataButton, 5, 15);
+            this.tableLayoutPanel1.Controls.Add(this.controlledHighButton, 5, 4);
+            this.tableLayoutPanel1.Controls.Add(this.controlledLowButton, 5, 5);
+            this.tableLayoutPanel1.Controls.Add(this.autoLowButton, 5, 3);
+            this.tableLayoutPanel1.Controls.Add(this.autoHighButton, 5, 2);
+            this.tableLayoutPanel1.Controls.Add(this.autonymousHighLabel, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.autonymousLowLabel, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.controlledHighLabel, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.controledLowLabel, 0, 5);
+            this.tableLayoutPanel1.Controls.Add(this.autoHighValueDisplay, 8, 2);
+            this.tableLayoutPanel1.Controls.Add(this.autoLowValueDisplay, 8, 3);
+            this.tableLayoutPanel1.Controls.Add(this.controlledHighValueDisplay, 8, 4);
+            this.tableLayoutPanel1.Controls.Add(this.controlledLowValueDisplay, 8, 5);
+            this.tableLayoutPanel1.Controls.Add(this.scoringLabel, 0, 1);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
@@ -80,7 +108,7 @@
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(544, 383);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(544, 437);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // teamColourLabel
@@ -90,7 +118,7 @@
             this.teamColourLabel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.teamColourLabel.Location = new System.Drawing.Point(3, 0);
             this.teamColourLabel.Name = "teamColourLabel";
-            this.teamColourLabel.Size = new System.Drawing.Size(96, 23);
+            this.teamColourLabel.Size = new System.Drawing.Size(96, 27);
             this.teamColourLabel.TabIndex = 0;
             this.teamColourLabel.Text = "Team Colour: ";
             this.teamColourLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -99,6 +127,7 @@
             // 
             this.tableLayoutPanel1.SetColumnSpan(this.teamColourComboBox, 5);
             this.teamColourComboBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.teamColourComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.teamColourComboBox.FormattingEnabled = true;
             this.teamColourComboBox.Location = new System.Drawing.Point(105, 3);
             this.teamColourComboBox.Name = "teamColourComboBox";
@@ -113,7 +142,7 @@
             this.matchNumberLabel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.matchNumberLabel.Location = new System.Drawing.Point(275, 0);
             this.matchNumberLabel.Name = "matchNumberLabel";
-            this.matchNumberLabel.Size = new System.Drawing.Size(96, 23);
+            this.matchNumberLabel.Size = new System.Drawing.Size(96, 27);
             this.matchNumberLabel.TabIndex = 2;
             this.matchNumberLabel.Text = "Match Number:";
             this.matchNumberLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -130,7 +159,7 @@
             0});
             this.matchNumberNumericUpDown.Name = "matchNumberNumericUpDown";
             this.matchNumberNumericUpDown.Size = new System.Drawing.Size(164, 20);
-            this.matchNumberNumericUpDown.TabIndex = 3;
+            this.matchNumberNumericUpDown.TabIndex = 2;
             this.matchNumberNumericUpDown.Value = new decimal(new int[] {
             1,
             0,
@@ -138,13 +167,181 @@
             0});
             this.matchNumberNumericUpDown.ValueChanged += new System.EventHandler(this.matchNumberNumericUpDown_ValueChanged);
             // 
+            // submitDataButton
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.submitDataButton, 6);
+            this.submitDataButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.submitDataButton.Location = new System.Drawing.Point(173, 408);
+            this.submitDataButton.Name = "submitDataButton";
+            this.submitDataButton.Size = new System.Drawing.Size(198, 26);
+            this.submitDataButton.TabIndex = 4;
+            this.submitDataButton.Text = "Submit Data";
+            this.submitDataButton.UseVisualStyleBackColor = true;
+            // 
+            // controlledHighButton
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.controlledHighButton, 3);
+            this.controlledHighButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.controlledHighButton.Location = new System.Drawing.Point(173, 111);
+            this.controlledHighButton.Name = "controlledHighButton";
+            this.controlledHighButton.Size = new System.Drawing.Size(96, 21);
+            this.controlledHighButton.TabIndex = 5;
+            this.controlledHighButton.Text = "Point Scored";
+            this.controlledHighButton.UseVisualStyleBackColor = true;
+            this.controlledHighButton.Click += new System.EventHandler(this.controlledHighButton_Click);
+            // 
+            // controlledLowButton
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.controlledLowButton, 3);
+            this.controlledLowButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.controlledLowButton.Location = new System.Drawing.Point(173, 138);
+            this.controlledLowButton.Name = "controlledLowButton";
+            this.controlledLowButton.Size = new System.Drawing.Size(96, 21);
+            this.controlledLowButton.TabIndex = 6;
+            this.controlledLowButton.Text = "Point Scored";
+            this.controlledLowButton.UseVisualStyleBackColor = true;
+            this.controlledLowButton.Click += new System.EventHandler(this.controlledLowButton_Click);
+            // 
+            // autoLowButton
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.autoLowButton, 3);
+            this.autoLowButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.autoLowButton.Location = new System.Drawing.Point(173, 84);
+            this.autoLowButton.Name = "autoLowButton";
+            this.autoLowButton.Size = new System.Drawing.Size(96, 21);
+            this.autoLowButton.TabIndex = 4;
+            this.autoLowButton.Text = "Point Scored";
+            this.autoLowButton.UseVisualStyleBackColor = true;
+            this.autoLowButton.Click += new System.EventHandler(this.autoLowButton_Click);
+            // 
+            // autoHighButton
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.autoHighButton, 3);
+            this.autoHighButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.autoHighButton.Location = new System.Drawing.Point(173, 57);
+            this.autoHighButton.Name = "autoHighButton";
+            this.autoHighButton.Size = new System.Drawing.Size(96, 21);
+            this.autoHighButton.TabIndex = 3;
+            this.autoHighButton.Text = "Point Scored";
+            this.autoHighButton.UseVisualStyleBackColor = true;
+            this.autoHighButton.Click += new System.EventHandler(this.autoHighButton_Click);
+            // 
+            // autonymousHighLabel
+            // 
+            this.autonymousHighLabel.AutoSize = true;
+            this.tableLayoutPanel1.SetColumnSpan(this.autonymousHighLabel, 5);
+            this.autonymousHighLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.autonymousHighLabel.Location = new System.Drawing.Point(3, 54);
+            this.autonymousHighLabel.Name = "autonymousHighLabel";
+            this.autonymousHighLabel.Size = new System.Drawing.Size(164, 27);
+            this.autonymousHighLabel.TabIndex = 9;
+            this.autonymousHighLabel.Text = "Autonymous High Points: ";
+            this.autonymousHighLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // autonymousLowLabel
+            // 
+            this.autonymousLowLabel.AutoSize = true;
+            this.tableLayoutPanel1.SetColumnSpan(this.autonymousLowLabel, 5);
+            this.autonymousLowLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.autonymousLowLabel.Location = new System.Drawing.Point(3, 81);
+            this.autonymousLowLabel.Name = "autonymousLowLabel";
+            this.autonymousLowLabel.Size = new System.Drawing.Size(164, 27);
+            this.autonymousLowLabel.TabIndex = 10;
+            this.autonymousLowLabel.Text = "Autonymous Low Points: ";
+            this.autonymousLowLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // controlledHighLabel
+            // 
+            this.controlledHighLabel.AutoSize = true;
+            this.tableLayoutPanel1.SetColumnSpan(this.controlledHighLabel, 5);
+            this.controlledHighLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.controlledHighLabel.Location = new System.Drawing.Point(3, 108);
+            this.controlledHighLabel.Name = "controlledHighLabel";
+            this.controlledHighLabel.Size = new System.Drawing.Size(164, 27);
+            this.controlledHighLabel.TabIndex = 11;
+            this.controlledHighLabel.Text = "Controlled High Points: ";
+            this.controlledHighLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // controledLowLabel
+            // 
+            this.controledLowLabel.AutoSize = true;
+            this.tableLayoutPanel1.SetColumnSpan(this.controledLowLabel, 5);
+            this.controledLowLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.controledLowLabel.Location = new System.Drawing.Point(3, 135);
+            this.controledLowLabel.Name = "controledLowLabel";
+            this.controledLowLabel.Size = new System.Drawing.Size(164, 27);
+            this.controledLowLabel.TabIndex = 12;
+            this.controledLowLabel.Text = "Controlled Low Points: ";
+            this.controledLowLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // autoHighValueDisplay
+            // 
+            this.autoHighValueDisplay.AutoSize = true;
+            this.tableLayoutPanel1.SetColumnSpan(this.autoHighValueDisplay, 3);
+            this.autoHighValueDisplay.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.autoHighValueDisplay.Location = new System.Drawing.Point(275, 54);
+            this.autoHighValueDisplay.Name = "autoHighValueDisplay";
+            this.autoHighValueDisplay.Size = new System.Drawing.Size(96, 27);
+            this.autoHighValueDisplay.TabIndex = 13;
+            this.autoHighValueDisplay.Text = "0";
+            this.autoHighValueDisplay.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // autoLowValueDisplay
+            // 
+            this.autoLowValueDisplay.AutoSize = true;
+            this.tableLayoutPanel1.SetColumnSpan(this.autoLowValueDisplay, 3);
+            this.autoLowValueDisplay.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.autoLowValueDisplay.Location = new System.Drawing.Point(275, 81);
+            this.autoLowValueDisplay.Name = "autoLowValueDisplay";
+            this.autoLowValueDisplay.Size = new System.Drawing.Size(96, 27);
+            this.autoLowValueDisplay.TabIndex = 14;
+            this.autoLowValueDisplay.Text = "0";
+            this.autoLowValueDisplay.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // controlledHighValueDisplay
+            // 
+            this.controlledHighValueDisplay.AutoSize = true;
+            this.tableLayoutPanel1.SetColumnSpan(this.controlledHighValueDisplay, 3);
+            this.controlledHighValueDisplay.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.controlledHighValueDisplay.Location = new System.Drawing.Point(275, 108);
+            this.controlledHighValueDisplay.Name = "controlledHighValueDisplay";
+            this.controlledHighValueDisplay.Size = new System.Drawing.Size(96, 27);
+            this.controlledHighValueDisplay.TabIndex = 15;
+            this.controlledHighValueDisplay.Text = "0";
+            this.controlledHighValueDisplay.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // controlledLowValueDisplay
+            // 
+            this.controlledLowValueDisplay.AutoSize = true;
+            this.tableLayoutPanel1.SetColumnSpan(this.controlledLowValueDisplay, 3);
+            this.controlledLowValueDisplay.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.controlledLowValueDisplay.Location = new System.Drawing.Point(275, 135);
+            this.controlledLowValueDisplay.Name = "controlledLowValueDisplay";
+            this.controlledLowValueDisplay.Size = new System.Drawing.Size(96, 27);
+            this.controlledLowValueDisplay.TabIndex = 16;
+            this.controlledLowValueDisplay.Text = "0";
+            this.controlledLowValueDisplay.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // scoringLabel
+            // 
+            this.scoringLabel.AutoSize = true;
+            this.tableLayoutPanel1.SetColumnSpan(this.scoringLabel, 11);
+            this.scoringLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.scoringLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoringLabel.Location = new System.Drawing.Point(3, 27);
+            this.scoringLabel.Name = "scoringLabel";
+            this.scoringLabel.Size = new System.Drawing.Size(368, 27);
+            this.scoringLabel.TabIndex = 17;
+            this.scoringLabel.Text = "Points Scored";
+            this.scoringLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // Aerial_Assist_UI_Layout_Test
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Aerial_Assist_UI_Layout_Test";
-            this.Size = new System.Drawing.Size(544, 383);
+            this.Size = new System.Drawing.Size(544, 437);
             this.Load += new System.EventHandler(this.Aerial_Assist_UI_Layout_Test_Load);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
@@ -160,5 +357,19 @@
         private System.Windows.Forms.ComboBox teamColourComboBox;
         private System.Windows.Forms.Label matchNumberLabel;
         private System.Windows.Forms.NumericUpDown matchNumberNumericUpDown;
+        private System.Windows.Forms.Button submitDataButton;
+        private System.Windows.Forms.Button controlledHighButton;
+        private System.Windows.Forms.Button controlledLowButton;
+        private System.Windows.Forms.Button autoLowButton;
+        private System.Windows.Forms.Button autoHighButton;
+        private System.Windows.Forms.Label autonymousHighLabel;
+        private System.Windows.Forms.Label autonymousLowLabel;
+        private System.Windows.Forms.Label controlledHighLabel;
+        private System.Windows.Forms.Label controledLowLabel;
+        private System.Windows.Forms.Label autoHighValueDisplay;
+        private System.Windows.Forms.Label autoLowValueDisplay;
+        private System.Windows.Forms.Label controlledHighValueDisplay;
+        private System.Windows.Forms.Label controlledLowValueDisplay;
+        private System.Windows.Forms.Label scoringLabel;
     }
 }
--- FRC_Scouting_V2/Test Objects/Aerial_Assist_UI_Layout_Test.cs
+++ FRC_Scouting_V2/Test Objects/Aerial_Assist_UI_Layout_Test.cs
@@ -6,13 +6,17 @@ namespace FRC_Scouting_V2.Test_Items
     //@author xNovax
     public partial class Aerial_Assist_UI_Layout_Test : UserControl
     {
+        private int autoHighTally;
+        private int autoLowTally;
+        private int controlledHighTally;
+        private int controlledLowTally;
+        private int matchNumber = 1;
+        private int teamColour;
+
         public Aerial_Assist_UI_Layout_Test()
         {
             InitializeComponent();
         }
-        //Variables
-        private int teamColour;
-        private int matchNumber = 1;
 
         private void teamColourComboBox_SelectedIndexChanged(object sender, EventArgs e)
         {
@@ -29,6 +33,14 @@ namespace FRC_Scouting_V2.Test_Items
             }
         }
 
+        public void UpdateLabels()
+        {
+            autoHighValueDisplay.Text = Convert.ToString(autoHighTally);
+            autoLowValueDisplay.Text = Convert.ToString(autoLowTally);
+            controlledHighValueDisplay.Text = Convert.ToString(controlledHighTally);
+            controlledLowValueDisplay.Text = Convert.ToString(controlledLowTally);
+        }
+
         private void Aerial_Assist_UI_Layout_Test_Load(object sender, EventArgs e)
         {
             teamColourComboBox.Items.Add("Blue");
@@ -38,6 +50,30 @@ namespace FRC_Scouting_V2.Test_Items
         private void matchNumberNumericUpDown_ValueChanged(object sender, EventArgs e)
         {
             matchNumber = Convert.ToInt32(matchNumberNumericUpDown.Value);
+        }
+
+        private void autoHighButton_Click(object sender, EventArgs e)
+        {
+            autoHighTally = autoHighTally + 1;
+            UpdateLabels();
+        }
+
+        private void autoLowButton_Click(object sender, EventArgs e)
+        {
+            autoLowTally = autoLowTally + 1;
+            UpdateLabels();
+        }
+
+        private void controlledHighButton_Click(object sender, EventArgs e)
+        {
+            controlledHighTally = controlledHighTally + 1;
+            UpdateLabels();
+        }
+
+        private void controlledLowButton_Click(object sender, EventArgs e)
+        {
+            controlledLowTally = controlledLowTally + 1;
+            UpdateLabels();
         }
     }
 }
