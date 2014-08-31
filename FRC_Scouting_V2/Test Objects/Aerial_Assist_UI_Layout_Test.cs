19d6e83cddec47d8a5679aea1a3a94d2e27b7fbc
xNovax <nova1104@live.com>
2014-08-30 22:10:51 -0400

    Code cleanup

--- FRC_Scouting_V2/Test Objects/Aerial_Assist_UI_Layout_Test.cs
+++ FRC_Scouting_V2/Test Objects/Aerial_Assist_UI_Layout_Test.cs
@@ -6,16 +6,16 @@ namespace FRC_Scouting_V2.Test_Items
     //@author xNovax
     public partial class Aerial_Assist_UI_Layout_Test : UserControl
     {
+        private int autoHighTally;
+        private int autoLowTally;
+        private int autoPickupTally;
+        private int controlledHighTally;
+        private int controlledLowTally;
+        private int controlledPickupTally;
+        private int hotGoalTally;
         private int matchNumber = 1;
+        private int missedPickupsTally;
         private int teamColour;
-        private int autoHighTally = 0;
-        private int autoLowTally = 0;
-        private int controlledHighTally = 0;
-        private int controlledLowTally = 0;
-        private int hotGoalTally = 0;
-        private int autoPickupTally = 0;
-        private int controlledPickupTally = 0;
-        private int missedPickupsTally = 0;
 
         public Aerial_Assist_UI_Layout_Test()
         {
@@ -34,31 +34,11 @@ namespace FRC_Scouting_V2.Test_Items
             missedPickupsTallyDisplay.Text = Convert.ToString(missedPickupsTally);
         }
 
-        private void teamColourComboBox_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            if (teamColourComboBox.SelectedIndex == 0)
-            {
-                teamColour = 0;
-            }
-            else
-            {
-                if (teamColourComboBox.SelectedIndex == 1)
-                {
-                    teamColour = 1;
-                }
-            }
-        }
-
         private void Aerial_Assist_UI_Layout_Test_Load(object sender, EventArgs e)
         {
             //Set team colour items
             teamColourComboBox.Items.Add("Blue");
             teamColourComboBox.Items.Add("Red");
-        }
-
-        private void matchNumberNumericUpDown_ValueChanged(object sender, EventArgs e)
-        {
-            matchNumber = Convert.ToInt32(matchNumberNumericUpDown.Value);
         }
 
         private void autoHighMinusButton_Click(object sender, EventArgs e)
@@ -82,6 +62,18 @@ namespace FRC_Scouting_V2.Test_Items
         private void autoLowPlusButton_Click(object sender, EventArgs e)
         {
             autoLowTally = autoLowTally + 1;
+            UpdateLabels();
+        }
+
+        private void autoPickupMinusButton_Click(object sender, EventArgs e)
+        {
+            autoPickupTally = autoPickupTally - 1;
+            UpdateLabels();
+        }
+
+        private void autoPickupPlusButton_Click(object sender, EventArgs e)
+        {
+            autoPickupTally = autoPickupTally + 1;
             UpdateLabels();
         }
 
@@ -109,30 +101,6 @@ namespace FRC_Scouting_V2.Test_Items
             UpdateLabels();
         }
 
-        private void hotGoalMinusButton_Click(object sender, EventArgs e)
-        {
-            hotGoalTally = hotGoalTally - 1;
-            UpdateLabels();
-        }
-
-        private void hotGoalPlusButton_Click(object sender, EventArgs e)
-        {
-            hotGoalTally = hotGoalTally + 1;
-            UpdateLabels();
-        }
-
-        private void autoPickupMinusButton_Click(object sender, EventArgs e)
-        {
-            autoPickupTally = autoPickupTally - 1;
-            UpdateLabels();
-        }
-
-        private void autoPickupPlusButton_Click(object sender, EventArgs e)
-        {
-            autoPickupTally = autoPickupTally + 1;
-            UpdateLabels();
-        }
-
         private void controllPickupMinusButton_Click(object sender, EventArgs e)
         {
             controlledPickupTally = controlledPickupTally - 1;
@@ -145,6 +113,23 @@ namespace FRC_Scouting_V2.Test_Items
             UpdateLabels();
         }
 
+        private void hotGoalMinusButton_Click(object sender, EventArgs e)
+        {
+            hotGoalTally = hotGoalTally - 1;
+            UpdateLabels();
+        }
+
+        private void hotGoalPlusButton_Click(object sender, EventArgs e)
+        {
+            hotGoalTally = hotGoalTally + 1;
+            UpdateLabels();
+        }
+
+        private void matchNumberNumericUpDown_ValueChanged(object sender, EventArgs e)
+        {
+            matchNumber = Convert.ToInt32(matchNumberNumericUpDown.Value);
+        }
+
         private void missedPickupsMinusButton_Click(object sender, EventArgs e)
         {
             missedPickupsTally = missedPickupsTally - 1;
@@ -155,6 +140,19 @@ namespace FRC_Scouting_V2.Test_Items
         {
             missedPickupsTally = missedPickupsTally + 1;
             UpdateLabels();
+        }
+
+        private void teamColourComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (teamColourComboBox.SelectedIndex)
+            {
+                case 0:
+                    teamColour = 0;
+                    break;
+                case 1:
+                    teamColour = 1;
+                    break;
+            }
         }
     }
 }
