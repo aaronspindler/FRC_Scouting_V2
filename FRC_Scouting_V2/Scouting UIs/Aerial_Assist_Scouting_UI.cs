747a4e45a9724dc2d61fa005fc82b3ad1deb9996
xNovax <nova1104@live.com>
2014-09-10 16:06:27 -0400

    Added table creation query to Aerial_Assist program

--- FRC_Scouting_V2/Scouting UIs/Aerial_Assist_Scouting_UI.cs
+++ FRC_Scouting_V2/Scouting UIs/Aerial_Assist_Scouting_UI.cs
@@ -253,6 +253,8 @@ namespace FRC_Scouting_V2
             }
 
             //MySQL Database
+
+            //MySQL Database Connection Info
             MySqlConnection conn;
             string databaseIP = Settings.Default.databaseIP;
             string databasePort = Settings.Default.databasePort;
@@ -263,9 +265,17 @@ namespace FRC_Scouting_V2
                 databaseIP, databasePort, databaseName, databaseUsername, databasePassword);
             try
             {
+                //Creating the connection to the database and opening the connection
                 conn = new MySqlConnection { ConnectionString = mySqlConnectionString };
                 conn.Open();
 
+                //Creating the table
+                const string query = ("CREATE TABLE `FRC_Scouting_Test` (`EntryID` int(11) NOT NULL,`TeamName` varchar(45) NOT NULL DEFAULT 'Default',`TeamNumber` int(11) NOT NULL DEFAULT '0',`TeamColour` varchar(45) NOT NULL DEFAULT 'Default',`MatchNumber` int(11) NOT NULL DEFAULT '0',`AutoHighTally` int(11) NOT NULL DEFAULT '0',`AutoLowTally` int(11) NOT NULL DEFAULT '0',`ControlledHighTally` int(11) NOT NULL DEFAULT '0',`ControlledLowTally` int(11) NOT NULL DEFAULT '0',`HotGoalTally` int(11) NOT NULL DEFAULT '0',`AutoPickup` int(11) NOT NULL DEFAULT '0',`ControlledPickup` int(11) NOT NULL DEFAULT '0',`MissedPickups` int(11) NOT NULL DEFAULT '0',`StartingLocationX` int(11) NOT NULL DEFAULT '0',`StartingLocationY` int(11) NOT NULL DEFAULT '0',PRIMARY KEY (`EntryID`)) ENGINE=InnoDB DEFAULT CHARSET=utf8");
+                var cmd = new MySqlCommand(query, conn);
+                cmd.ExecuteNonQuery();
+                //end of creating the table
+
+                //Closing the connection
                 conn.Close();
             }
             catch (MySqlException ex)
