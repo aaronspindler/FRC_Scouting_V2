71e65bf823d8006333aa8a979bebe54f8b5955ad
xNovax <nova1104@live.com>
2014-09-12 10:53:29 -0400

    Added Aerial_Assist_Stats_UI

---
+++ FRC_Scouting_V2/UIs/Aerial_Assist_Stats_UI.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FRC_Scouting_V2.UIs
+{
+    public partial class Aerial_Assist_Stats_UI : UserControl
+    {
+        public Aerial_Assist_Stats_UI()
+        {
+            InitializeComponent();
+        }
+    }
+}
