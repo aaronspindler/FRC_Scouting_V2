04a33de465e0a124cfe22a26a9651b49e4d99c8f
xNovax <nova1104@live.com>
2014-09-21 15:03:29 -0400

    Sync

--- FRC_Scouting_V2/UsefulSnippets.cs
+++ FRC_Scouting_V2/UsefulSnippets.cs
@@ -59,13 +59,12 @@ namespace FRC_Scouting_V2
             {
                 try
                 {
-                    string mySqlConnectionString =
-                        String.Format("Server={0};Port={1};Database={2};Uid={3};password={4};",
-                            Settings.Default.databaseIP, Settings.Default.databasePort, Settings.Default.databaseName,
-                            Settings.Default.databaseUsername, Settings.Default.databasePassword);
+                    string mySqlConnectionString =String.Format("Server={0};Port={1};Database={2};Uid={3};password={4};",Settings.Default.databaseIP, Settings.Default.databasePort, Settings.Default.databaseName,Settings.Default.databaseUsername, Settings.Default.databasePassword);
                     var conn = new MySqlConnection { ConnectionString = mySqlConnectionString };
-                    var cmd = new MySqlCommand(("SELECT * FROM " + tableName), conn);
-                    MySqlDataReader dr = cmd.ExecuteReader();
+                    var cmd = new MySqlCommand();
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
                 }
                 catch (MySqlException ex)
                 {
