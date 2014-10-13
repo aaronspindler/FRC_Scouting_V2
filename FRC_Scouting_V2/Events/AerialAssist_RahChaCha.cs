658988677b3745624fa7b16199ba7aa2c26a3739
xNovax <nova1104@live.com>
2014-10-13 17:33:57 -0400

    Changed up the standard deviation method

--- FRC_Scouting_V2/Events/AerialAssist_RahChaCha.cs
+++ FRC_Scouting_V2/Events/AerialAssist_RahChaCha.cs
@@ -197,18 +197,16 @@ namespace FRC_Scouting_V2
         private double CalculateStdDev(IEnumerable<double> values)
         {
             double ret = 0;
+            var enumerable = values as double[] ?? values.ToArray();
+            if (!enumerable.Any()) return ret;
+            //Compute the Average
+            var avg = enumerable.Average();
 
-            if (values.Count() > 0)
-            {
-                //Compute the Average
-                double avg = values.Average();
+            //Perform the Sum of (value-avg)^2
+            var sum = enumerable.Sum(d => Math.Pow(d - avg, 2));
 
-                //Perform the Sum of (value-avg)^2
-                double sum = values.Sum(d => Math.Pow(d - avg, 2));
-
-                //Put it all together
-                ret = Math.Sqrt((sum)/values.Count() - 1);
-            }
+            //Put it all together
+            ret = Math.Sqrt((sum)/enumerable.Count() - 1);
             return ret;
         }
 
