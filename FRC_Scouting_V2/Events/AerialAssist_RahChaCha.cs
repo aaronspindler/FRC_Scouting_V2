a03d6fb4f332d23f7c3e16c9861587eb8f1baa4b
xNovax <nova1104@live.com>
2014-10-14 13:23:05 -0400

    Sync

--- FRC_Scouting_V2/Events/AerialAssist_RahChaCha.cs
+++ FRC_Scouting_V2/Events/AerialAssist_RahChaCha.cs
@@ -42,6 +42,7 @@ namespace FRC_Scouting_V2
     public partial class AerialAssist_RahChaCha : Form
     {
         private const string TABLE_NAME = ("AerialAssist_RahChaCha");
+
         private const int HIGH_GOAL_VALUE = 10;
         private const int LOW_GOAL_VALUE = 1;
         private const int BALL_CATCH_VALUE = 10;
@@ -49,6 +50,7 @@ namespace FRC_Scouting_V2
         private const int TRIPLE_ASSIST_GOAL_VALUE = 30;
         private const int AUTO_MOBILITY_VALUE = 5;
         private const int AUTO_ADDITIONAL_POINTS_VALUE = 5;
+
         private readonly double[] _autoHighGoalSuccessRate = new double[2];
         private readonly double[] _autoHighMean = new double[2];
         private readonly double[] _autoHighStandardDeviation = new double[2];
