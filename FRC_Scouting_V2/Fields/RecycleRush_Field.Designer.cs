4dd39bd5372b379b7431e4d0d1a32abbac6475d4
xNovax <nova1104@live.com>
2015-01-11 15:06:59 -0500

    Resized AerialAssist events and added field files

--- FRC_Scouting_V2/Fields/RecycleRush_Field.Designer.cs
+++ FRC_Scouting_V2/Fields/RecycleRush_Field.Designer.cs
@@ -29,34 +29,56 @@
         private void InitializeComponent()
         {
             this.fieldPanel = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // fieldPanel
             // 
             this.fieldPanel.AutoSize = true;
             this.fieldPanel.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tableLayoutPanel1.SetColumnSpan(this.fieldPanel, 2);
             this.fieldPanel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.fieldPanel.Location = new System.Drawing.Point(0, 0);
+            this.fieldPanel.Location = new System.Drawing.Point(2, 2);
+            this.fieldPanel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.fieldPanel.Name = "fieldPanel";
-            this.fieldPanel.Size = new System.Drawing.Size(1442, 873);
+            this.fieldPanel.Size = new System.Drawing.Size(1038, 517);
             this.fieldPanel.TabIndex = 0;
             this.fieldPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.fieldPanel_Paint);
             // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.fieldPanel, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1042, 546);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
             // RecycleRush_Field
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.Controls.Add(this.fieldPanel);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "RecycleRush_Field";
-            this.Size = new System.Drawing.Size(1442, 873);
+            this.Size = new System.Drawing.Size(1042, 546);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Panel fieldPanel;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
     }
 }
