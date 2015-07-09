30d3dfee2410db3b6b3df8ad2704790c1a5d12ee
xNovax <nova1104@live.com>
2015-07-09 18:23:17 -0400

    Added SecurityUnitTests for Encryptor and Decryptor

--- UsefulSnippets.Test/SecurityUnitTests.cs
+++ UsefulSnippets.Test/SecurityUnitTests.cs
@@ -7,8 +7,20 @@ namespace UsefulSnippets.Test
     public class SecurityUnitTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void EncryptStringTestMethod()
         {
+            string expectedData = "sMRmMCcS8gvWQzAfAQBCCg==";
+            string actualData = Security.EncryptString("FRC_Scouting_V2");
+
+            Assert.AreEqual(expectedData,actualData,"Encryption is not working correctly");
+        }
+
+        [TestMethod]
+        public void DecryptStringTestMethod()
+        {
+            string expectedData = "FRC_Scouting_V2";
+            string actualData = Security.DeCryptString("sMRmMCcS8gvWQzAfAQBCCg==");
+            Assert.AreEqual(expectedData,actualData,"Decryption is not working correctly");
         }
     }
 }
