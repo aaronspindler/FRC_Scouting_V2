00969e6b154af8321ba01fa108d021c66a51b224
xNovax <nova1104@live.com>
2015-07-20 20:07:18 -0400

    Updated license headers to be automated

--- FRC_Scouting_V2.Desktop/Models/RecycleRush_Pit_Scouting_Team.cs
+++ FRC_Scouting_V2.Desktop/Models/RecycleRush_Pit_Scouting_Team.cs
@@ -1,4 +1,5 @@
-﻿//*********************************License***************************************
+﻿#region License
+//*********************************License***************************************
 //===============================================================================
 //The MIT License (MIT)
 
@@ -22,7 +23,7 @@
 //OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 //SOFTWARE.
 //===============================================================================
-
+#endregion
 using System;
 
 namespace FRC_Scouting_V2
--- UsefulSnippets/Notifications.cs
+++ UsefulSnippets/Notifications.cs
@@ -1,4 +1,5 @@
-﻿//*********************************License***************************************
+﻿#region License
+//*********************************License***************************************
 //===============================================================================
 //The MIT License (MIT)
 
@@ -22,7 +23,7 @@
 //OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 //SOFTWARE.
 //===============================================================================
-
+#endregion
 using System;
 using System.Windows.Forms;
 using CrashReporterDotNET;
