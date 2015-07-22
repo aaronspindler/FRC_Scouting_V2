923c5dc1818ffbf5592efd8bfa58655be3856e2d
xNovax <aaron@xnovax.net>
2015-07-21 23:36:59 -0400

    Added Nunit

--- UsefulSnippets.Test/NetworkUnitTests.cs
+++ UsefulSnippets.Test/NetworkUnitTests.cs
@@ -4,16 +4,16 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace UsefulSnippets.Test
 {
-    [TestClass]
-    public class NetworkUnitTests
-    {
-        [TestMethod]
-        public void CheckForInternetConnectionTestMethod()
-        {
-            Boolean actualData = Network.CheckForInternetConnection();
-            const bool expectedData = true;
+    //[TestClass]
+    //public class NetworkUnitTests
+    //{
+    //    [TestMethod]
+    //    public void CheckForInternetConnectionTestMethod()
+    //    {
+    //        Boolean actualData = Network.CheckForInternetConnection();
+    //        const bool expectedData = true;
 
-            Assert.AreEqual(expectedData, actualData, "Internet connectivity check is not as expected");
-        }
-    }
+    //        Assert.AreEqual(expectedData, actualData, "Internet connectivity check is not as expected");
+    //    }
+    //}
 }
