7ea6410eb35660d7da17a1fed6c207e4e19ce62a
xNovax <aaron@xnovax.net>
2015-07-25 16:06:16 -0400

    Started to convert winforms program into a WPF program

---
+++ FRC_Scouting_V2.WPF/Windows/Home.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace FRC_Scouting_V2.WPF
+{
+    /// <summary>
+    /// Interaction logic for Home.xaml
+    /// </summary>
+    public partial class Home : Window
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void eventSelector_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            switch (eventSelector.SelectedIndex)
+            {
+                case 0:
+                    MessageBox.Show("0");
+                    break;
+                case 1:
+                    MessageBox.Show("1");
+                    break;
+            }
+        }
+    }
+}
