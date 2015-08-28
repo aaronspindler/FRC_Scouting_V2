d52c15a0f24341fa1d03ca97bd607fc150c97b51
xNovax <aaron@xnovax.net>
2015-08-28 17:20:59 -0400

    Removed FRC_Scouting.Web

--- FRC_Scouting.Web/Account/ResetPasswordConfirmation.aspx.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Web.UI;
-
-namespace FRC_Scouting.Web.Account
-{
-    public partial class ResetPasswordConfirmation : Page
-    {
-    }
-}
--- FRC_Scouting.Web/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FRC_Scouting.Web.Global" Language="C#" %>
