efa310f3db093a03d153fba9b6fc7aa390f709df
xNovax <aaron@xnovax.net>
2016-01-26 00:38:00 -0500

    Added Tab Pages

--- FRC_Scouting_V2.Desktop/Events/2016_Stronghold/RegularSeason/GTR_East/Stronghold_GTR_East.Designer.cs
+++ FRC_Scouting_V2.Desktop/Events/2016_Stronghold/RegularSeason/GTR_East/Stronghold_GTR_East.Designer.cs
@@ -35,8 +35,8 @@
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.mainTeamSelector = new System.Windows.Forms.ComboBox();
             this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.matchScoutingTabPage = new System.Windows.Forms.TabPage();
+            this.pitScoutingTabPage = new System.Windows.Forms.TabPage();
             this.mainTableLayout.SuspendLayout();
             this.mainMenuStrip.SuspendLayout();
             this.tabControl1.SuspendLayout();
@@ -82,7 +82,7 @@
             // exitToolStripMenuItem
             // 
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
             this.exitToolStripMenuItem.Text = "Exit";
             // 
             // mainTeamSelector
@@ -97,8 +97,8 @@
             // tabControl1
             // 
             this.mainTableLayout.SetColumnSpan(this.tabControl1, 2);
-            this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.matchScoutingTabPage);
+            this.tabControl1.Controls.Add(this.pitScoutingTabPage);
             this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tabControl1.Location = new System.Drawing.Point(3, 26);
             this.tabControl1.Name = "tabControl1";
@@ -106,25 +106,25 @@
             this.tabControl1.Size = new System.Drawing.Size(1012, 510);
             this.tabControl1.TabIndex = 2;
             // 
-            // tabPage1
+            // matchScoutingTabPage
             // 
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(1004, 484);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
-            this.tabPage1.UseVisualStyleBackColor = true;
+            this.matchScoutingTabPage.Location = new System.Drawing.Point(4, 22);
+            this.matchScoutingTabPage.Name = "matchScoutingTabPage";
+            this.matchScoutingTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.matchScoutingTabPage.Size = new System.Drawing.Size(1004, 484);
+            this.matchScoutingTabPage.TabIndex = 0;
+            this.matchScoutingTabPage.Text = "Match Scouting";
+            this.matchScoutingTabPage.UseVisualStyleBackColor = true;
             // 
-            // tabPage2
+            // pitScoutingTabPage
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(192, 74);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
-            this.tabPage2.UseVisualStyleBackColor = true;
+            this.pitScoutingTabPage.Location = new System.Drawing.Point(4, 22);
+            this.pitScoutingTabPage.Name = "pitScoutingTabPage";
+            this.pitScoutingTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.pitScoutingTabPage.Size = new System.Drawing.Size(1004, 484);
+            this.pitScoutingTabPage.TabIndex = 1;
+            this.pitScoutingTabPage.Text = "Pit Scouting";
+            this.pitScoutingTabPage.UseVisualStyleBackColor = true;
             // 
             // Stronghold_GTR_East
             // 
@@ -153,7 +153,7 @@
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
         private System.Windows.Forms.ComboBox mainTeamSelector;
         private System.Windows.Forms.TabControl tabControl1;
-        private System.Windows.Forms.TabPage tabPage1;
-        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage matchScoutingTabPage;
+        private System.Windows.Forms.TabPage pitScoutingTabPage;
     }
 }
